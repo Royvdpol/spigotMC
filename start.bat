5c4069dbdcafefd7280f20d5ef1cadcc60df1b09
royvdpol <royvdpol@gmail.com>
2024-09-12 00:18:02 +0200

    use jdk-17

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @Echo off
-"C:\Program Files\Java\jdk-17\bin\java.exe" -Xms1G -Xmx2G -jar spigot-<version>.jar nogui
+"C:\Program Files\Java\jdk-17\bin\java.exe" -Xms1G -Xmx2G -jar spigot-1.21.1.jar nogui
 pause
